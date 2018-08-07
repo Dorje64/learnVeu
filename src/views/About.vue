2f183734b411490b20ad89cfe67b39e6484dca72
Dorje <ku.dorjee@gmail.com>
2018-08-07 14:19:13 +0545

    played around

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about pag</h1>
+    <Analysis msg="Welcome to the Jungle"/>
   </div>
 </template>
+
+<script>
+import Analysis from "@/components/Analysis.vue";
+
+export default {
+  name: "home",
+  components: {
+    Analysis
+  }
+};
+</script>
