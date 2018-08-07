406bdba5a35380ba5f01d48c70954a8ec9174089
Dorje <ku.dorjee@gmail.com>
2018-08-07 14:18:17 +0545

    import libraries

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// import BootstrapVue from 'bootstrap-vue'
+
+// Vue.use(BootstrapVue);
+
+import 'bootstrap/dist/css/bootstrap.css'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+//
+// library.add(faCoffee)
+//
+// Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
 
 Vue.config.productionTip = false;
 
